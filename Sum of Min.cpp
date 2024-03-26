b1c77e6cbe1e70c76fec72cf5b9fdca0a516ae7a
Anadi Sharma <142089653+HaiL555@users.noreply.github.com>
2024-03-26 23:31:33 +0530

    Add files via upload

---
+++ Sum of Min.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() 
+{
+  int n;
+  printf("Enter the total number of elements: ");
+  scanf("%d", &n);
+  int ar[n];
+  printf("Enter the array elements: ");
+  for (int i = 0; i < n; i++) 
+  {
+    scanf("%d", &ar[i]);
+  }
+  int min = ar[0] + ar[1];    // Taking Min sum of the firs 2 elements initially
+  for (int i = 0; i < n; i++) 
+  {
+    for (int j = i + 1; j < n; j++) 
+	{
+      int currentSum = ar[i] + ar[j];
+      if (currentSum < min) 
+	  {
+        min = currentSum;
+      }
+    }
+  }
+  printf("The minimum sum of any two elements is: %d\n", min);
+  return 0;
+}
